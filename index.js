a80eebe6ff52aa22eeb152c1de95f5cf0e7d0e73
@wisofer 17 <87929107+Wisofer@users.noreply.github.com>
2023-11-23 09:15:20 -0600

    nueva version

--- index.js
+++ index.js
@@ -5,11 +5,7 @@ import { Server as SocketServer } from "socket.io";
 const app = express();
 const server = http.createServer(app);
 
-const io = new SocketServer(server, {
-  cors: {
-    origin: "https://frontend-wiso.netlify.app"
-  }
-});
+const io = new SocketServer(server);
 
 io.on("connection", socket => {
   console.log("Cliente Conectado");
